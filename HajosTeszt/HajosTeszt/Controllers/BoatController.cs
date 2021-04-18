763b2480fb13fe7533012a96329a67c1e9b5a476
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-04-18 09:55:42 +0200

    8.

---
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -0,0 +1,29 @@
+﻿using HajosTeszt.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    public class BoatController : Controller
+    {
+
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            hajostesztContext context = new hajostesztContext();
+            var kérdések = from x in context.Questions select x.QuestionText;
+
+            return new JsonResult(kérdések);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
