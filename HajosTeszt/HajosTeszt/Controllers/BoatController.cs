541adde7da9d4a2f79ff0a7e3c00d2aeda1e8131
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-04-24 22:07:12 +0200

    9.

--- HajosTeszt/HajosTeszt/Controllers/BoatController.cs
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -26,4 +26,23 @@ namespace HajosTeszt.Controllers
             return View();
         }
     }
+
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
+    }
 }
--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -2,6 +2,7 @@
 var kérdések;
 var aktuális;
 var maximum;
+//var nincsképkép;
 
 function letöltésBefejeződött(d) {
     console.log("Sikeres letöltés")
@@ -52,5 +53,52 @@ document.getElementById("válasz1").click = function () {
     
 }
 
+fetch('/questions/4')
+    .then(response => response.json())
+    .then(data => console.log(data)
+);
+
+fetch('/questions/1')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+    );
+
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+    if (document.getElementById("kép").src.exists()) {
+        
+    }
+    else {
+        //document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + nincsképkép;
+        document.getElementById("kép").remove();
+    }
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
+
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
+    }
+}
 
 window.onload = () => { letoltes(); aktuális = 0; kérdésMegjelenítés(aktuális);  }
