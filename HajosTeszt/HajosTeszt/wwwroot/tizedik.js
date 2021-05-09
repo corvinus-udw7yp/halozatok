a64f9f9bfe53020985a28c0d3ff63b7c1f110b4e
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-05-09 19:10:26 +0200

    10.1

---
+++ HajosTeszt/HajosTeszt/wwwroot/tizedik.js
@@ -0,0 +1,29 @@
+﻿var hotList = [];               //Az éppen gyakoroltatott kérdések listája
+var questionsInHotList = 3;     //Ez majd 7 lesz, teszteléshez jobb a 3.
+var displayedQuestion;          //A hotList-ből éppen ez a kérdés van kint
+var numberOfQuestions;          //Kérdések száma a teljes adatbázisban
+var nextQuestion = 1;           //A következő kérdés száma a teljes listában
+
+function init() {
+    for (var i = 0; i < questionsInHotList; i++) {
+        let q = {
+            question: {},
+            goodAnswers : 0
+
+        }
+
+        hotList[i] = q;
+    }
+
+    //Kezdőkérdéslista letöltése
+    for (var i = 0; i < questionsInHotList; i++) {
+        kérdésBetöltés(nextQuestion, i);
+        nextQuestion++;
+    }
+
+}
+
+function kérdésBetöltés(questionNumber, destination) {
+    fetch(`/questions/${questionNumber}`)   
+    .then()
+}
